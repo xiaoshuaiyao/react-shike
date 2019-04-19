3cce4743b2b02aa1fe841c2ccd40f198685b176f
xiaoshuaiyao <1042799677@qq.com>
2019-04-19 14:34:15 +0800

    修改

--- src/views/index.js
+++ src/views/index.js
@@ -3,11 +3,12 @@ import Class from "./class"
 import Mine from "./mine"
 import Shopping from "./shopping"
 import Order from "./order"
-
+import SetUp from "./mine/setUp"
 export {
     Home,
     Class,
     Mine,
     Shopping,
-    Order
+    Order,
+    SetUp
 }
---
+++ src/views/mine/setUp/index.js
@@ -0,0 +1,4 @@
+import SetUp from "./mine/setUp";
+
+
+export default SetUp
